f613581183a6763d2227c56fcbb9aa047aeef2bc
mojeda <martuchi.ojeda@gmail.com>
2019-01-30 19:42:00 +0100

    Session 1

--- P0/sum.py
+++ P0/sum.py
@@ -1,1 +1,6 @@
+print("This program will print the adding of numbers from 1 to 100.")
 
+sum_total = 0
+for e in range(1,101):
+    sum_total = sum_total + e
+print("The total sum of the numbers is:", sum_total)
