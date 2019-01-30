c7b469120ed7a431e34adb63058897b160f776f1
mojeda <martuchi.ojeda@gmail.com>
2019-01-30 20:24:43 +0100

    Prueba 1

--- P0/exercise-4.py
+++ P0/exercise-4.py
@@ -1,11 +1,3 @@
-sequence = input("Introduce a sequence:")
-sequence = sequence.upper()
-print("The of the sequence is:", len(sequence))
-
-print("The letter A appears", sequence.count("A"), "times.")
-print("The letter C appears", sequence.count("C"), "times.")
-print("The letter G appears", sequence.count("G"), "times.")
-print("The letter T appears", sequence.count("T"), "times.")
 
 
 
