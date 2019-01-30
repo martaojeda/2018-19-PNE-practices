05704203e4f4aeeb416ddf8ba9c105765b3b7e99
mojeda <martuchi.ojeda@gmail.com>
2019-01-30 19:44:48 +0100

    Session 1

--- P0/sum.py
+++ P0/sum.py
@@ -1,6 +1,6 @@
 print("This program will print the adding of numbers from 1 to 100.")
 
-sum_total = 0
-for e in range(1,101):
-    sum_total = sum_total + e
-print("The total sum of the numbers is:", sum_total)
+sum = 0
+for e in range(100):
+    sum = sum + e
+print("The sum of the first 100 integer numbers is:", sum)
