e7ac0a1da1acc36d5beba6a1b930cdcf420a0c4e
mojeda <martuchi.ojeda@gmail.com>
2019-02-01 15:51:30 +0100

    Session 2

--- P0/dna_count_file.py
+++ P0/dna_count_file.py
@@ -1,19 +1,10 @@
 file = input("Please, introduce the name of the file:")
-data = open(file, "r")
-count_a = 0
-count_c = 0
-count_g = 0
-count_t = 0
-length = 0
-for line in data:
-    line.replace(" ","" ).upper()
-    count_a += line.count("A")
-    count_c += line.count("C")
-    count_g += line.count("G")
-    count_t += line.count("T")
-    length += len(line.replace(" ", "").replace("\n", ""))
-print("The length of the sequence is:", length)
-print("The letter A appears ", count_a, "times.")
-print("The letter C appears ", count_c, "times.")
-print("The letter G appears ", count_g, "times.")
-print("The letter T appears ", count_t, "times.")
+with open (file, "r") as f:
+    sequences = f.read()
+    sequences = sequences.replace(" ", "")
+    print("The length of the sequence is:", len(sequences))
+    print("The letter A appears ", sequences.count("A"), "times.")
+    print("The letter C appears ", sequences.count("C"), "times.")
+    print("The letter G appears ", sequences.count("G"), "times.")
+    print("The letter T appears ", sequences.count("T"), "times.")
+
