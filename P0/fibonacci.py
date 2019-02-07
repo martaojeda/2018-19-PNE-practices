ac8aec1b8369a42425120e680e8e61df02df318c
mojeda <martuchi.ojeda@gmail.com>
2019-02-07 10:57:44 +0100

    Session 1 and 2

--- P0/fibonacci.py
+++ P0/fibonacci.py
@@ -1,8 +1,11 @@
 n = input("Introduce a number:")
 n = int(n)
 def fibonacci(n):
-    a, b = 1, 1
-    for e in range(n-2):
-        a, b =b, a+b
-    return a
-print(fibonacci(n))
+    a, b = 0, 1
+    while a < n:
+        print(a, end='')
+        a, b = b, a+b
+
+    print()
+    return
+fibonacci(n)
