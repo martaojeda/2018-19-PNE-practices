36464a2aef95a5510f3627aa21d72a1c25fa0072
mojeda <martuchi.ojeda@gmail.com>
2019-02-13 17:35:03 +0100

    Practice 2

--- P2/client.py
+++ P2/client.py
@@ -7,9 +7,17 @@ while True:
     sequence = input("Introduce a valid sequence:")
     s1 = Seq(sequence)
     s2 = s1.complement()
+    s3 = s1.reverse()
+    send_1 = "The complement sequence is:" + s2.strbases
+    send_2 = "\nThe reverse sequence is:" + s3.strbases
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.connect((IP, PORT))
-    s.send(str.encode(s2.strbases))
+    s.send(str.encode(send_1))
+    s.send(str.encode(send_2))
+
+
     msg = s.recv(2048).decode("utf-8")
-    print(msg)
+
+    print("Message from server:{}".format(msg))
+
     s.close()
