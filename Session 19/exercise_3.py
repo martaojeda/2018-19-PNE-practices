180efd0d5b62242eab63aa16ba981be2e7354ff3
mojeda <martuchi.ojeda@gmail.com>
2019-03-30 20:35:59 +0100

    Session 19

---
+++ Session 19/exercise_3.py
@@ -0,0 +1,78 @@
+import http.client
+import json
+
+# -- API information
+HOSTNAME = "api.github.com"
+ENDPOINT = "/users/"
+GITHUB_ID = input("Introduce the user you want to know the information:")
+METHOD = "GET"
+
+# -- Here we can define special headers if needed
+headers = {'User-Agent': 'http-client'}
+
+# -- Connect to the server
+# -- NOTICE it is an HTTPS connection!
+# -- If we do not specify the port, the standar one
+# -- will be used
+conn = http.client.HTTPSConnection(HOSTNAME)
+
+# -- Send the request. No body (None)
+# -- Use the defined headers
+conn.request(METHOD, ENDPOINT + GITHUB_ID, None, headers)
+
+# -- Wait for the server's response
+r1 = conn.getresponse()
+
+# -- Print the status
+print()
+print("Response received: ", end='')
+print(r1.status, r1.reason)
+
+# -- Read the response's body and close
+# -- the connection
+text_json = r1.read().decode("utf-8")
+conn.close()
+
+# -- Optionally you can print the
+# -- received json file for testing
+# print(text_json)
+
+# -- Generate the object from the json file
+user = json.loads(text_json)
+
+# -- Get some data
+login = user['login']
+name = user['name']
+bio = user['bio']
+nrepos = user['public_repos']
+
+print()
+print("User: {}".format(login))
+print("Name: {}".format(name))
+print("Repos: {}".format(nrepos))
+print("Bio: \n{}".format(bio))
+
+
+#This is to know the name of the repositories
+conn = http.client.HTTPSConnection(HOSTNAME)
+conn.request(METHOD, ENDPOINT + GITHUB_ID +"/repos", None, headers)
+r1 = conn.getresponse()
+print()
+print("Response received: ", end='')
+print(r1.status, r1.reason)
+text_json = r1.read().decode("utf-8")
+conn.close()
+repos_list = json.loads(text_json)
+for repo in repos_list:
+    print("The name of the repos is:{}".format(repo['name']))
+
+
+ENDPOINT = "/repos/"+GITHUB_ID+"/2018-19-PNE-practices/commits"
+conn = http.client.HTTPSConnection(HOSTNAME)
+conn.request(METHOD, ENDPOINT, None, headers)
+r1 = conn.getresponse()
+text_json = r1.read().decode("utf-8")
+conn.close()
+repos_list = json.loads(text_json)
+print("Total commits:", len(repos_list))
+
