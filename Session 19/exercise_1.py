b60a1018f7f555d2e939e59eff49ba9d61a1d267
mojeda <martuchi.ojeda@gmail.com>
2019-03-30 20:34:54 +0100

    Session 19

---
+++ Session 19/exercise_1.py
@@ -0,0 +1,105 @@
+
+
+import http.client
+import json
+
+# -- API information
+HOSTNAME = "api.icndb.com"
+ENDPOINT = "/jokes/random"
+METHOD = "GET"
+
+# -- Here we can define special headers if needed
+headers = {'User-Agent': 'http-client'}
+
+# -- Connect to the server
+# -- NOTICE it is an HTTPS connection!
+# -- If we do not specify the port, the standar one
+# -- will be used
+conn = http.client.HTTPSConnection(HOSTNAME)
+
+# -- Send the request. No body (None)
+# -- Use the defined headers
+conn.request(METHOD, ENDPOINT, None, headers)
+
+# -- Wait for the server's response
+r1 = conn.getresponse()
+
+# -- Print the status
+print()
+print("Response received: ", end='')
+print(r1.status, r1.reason)
+
+# -- Read the response's body and close
+# -- the connection
+text_json = r1.read().decode("utf-8")
+conn.close()
+
+# -- Optionally you can print the
+# -- received json file for testing
+#print(text_json)
+
+# -- Generate the object from the json file
+joke = json.loads(text_json)
+print("The joke is:", joke['value']['joke'])
+
+#This is to count the number of jokes of the program.
+ENDPOINT = "/jokes/count"
+
+conn = http.client.HTTPSConnection(HOSTNAME)
+
+# -- Send the request. No body (None)
+# -- Use the defined headers
+conn.request(METHOD, ENDPOINT, None, headers)
+
+# -- Wait for the server's response
+r1 = conn.getresponse()
+
+# -- Print the status
+print()
+print("Response received: ", end='')
+print(r1.status, r1.reason)
+
+# -- Read the response's body and close
+# -- the connection
+text_json = r1.read().decode("utf-8")
+conn.close()
+
+# -- Optionally you can print the
+# -- received json file for testing
+#print(text_json)
+
+# -- Generate the object from the json file
+Total_jokes = json.loads(text_json)
+print("The number of total jokes is:", Total_jokes['value'])
+
+#This is to know the different categories an the total number.
+ENDPOINT = "/categories"
+
+conn = http.client.HTTPSConnection(HOSTNAME)
+
+# -- Send the request. No body (None)
+# -- Use the defined headers
+conn.request(METHOD, ENDPOINT, None, headers)
+
+# -- Wait for the server's response
+r1 = conn.getresponse()
+
+# -- Print the status
+print()
+print("Response received: ", end='')
+print(r1.status, r1.reason)
+
+# -- Read the response's body and close
+# -- the connection
+text_json = r1.read().decode("utf-8")
+conn.close()
+
+# -- Optionally you can print the
+# -- received json file for testing
+#print(text_json)
+
+# -- Generate the object from the json file
+categories = json.loads(text_json)
+print("The number of total categories is:", len(categories['value']))
+for i in categories['value']:
+    print(i)
